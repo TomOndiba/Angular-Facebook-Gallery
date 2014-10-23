4e802bc31562d3f75a537903eee228738600f4fe
Rakshit Shukla <shuklapossible@gmail.com>
2014-10-23 00:33:15 -0400

    added css transition on gallery load

--- public/controllers/gallery-ctrl.js
+++ public/controllers/gallery-ctrl.js
@@ -5,9 +5,11 @@ angular.module('AppControllers')
 
 .controller('GalleryCtrl', function ($scope, FacebookService, $log) {
 
+	$scope.loaded = false;
         FacebookService.getFacebookData()
           .then(function(data) {
           	/* To remove undefined entries */
+          	$scope.loaded = true;
             data = _.compact(data);
             $scope.albums = data;
         });
